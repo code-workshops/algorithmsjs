1888d1d772414f7eea49c7250b45bd4dec202a9c
Dev <dev@divethree.com>
2018-03-13 11:52:17 -0700

    Add more challenges and tests.

--- roundOne.js
+++ roundOne.js
@@ -2,6 +2,7 @@
  * Lead with Zeroes
  * @description: Returns the same array, but moves all zeroes in the array to the front.
  * Do not return a new array. It must be the same array!
+ *      Ex: leadZeroes([2,2,0,1,0]) => [0,0,2,2,1]
  *
  * @param {array} arr - An array of integers
  */
@@ -12,8 +13,9 @@ function leadZeroes(arr) {
 /**
  * Setter
  * @description: Returns an array of unique numbers.
- *      Ex.: setter([1,1,2,2,3,3]) => [1,2,3]
- * @param arr
+ *      Ex: setter([1,1,2,2,3,3]) => [1,2,3]
+ *      
+ * @param {array} arr - An array of data
  */
 function setter(arr) {
 
@@ -22,7 +24,7 @@ function setter(arr) {
 /**
  * Is Prime
  * @description: Return true if the given number is a prime number, false otherwise
- *      Ex.: isPrime(3) => true
+ *      Ex: isPrime(3) => true
  * @param num
  */
 function isPrime(num) {
@@ -32,7 +34,8 @@ function isPrime(num) {
 /**
  * Modulus
  * @description: Write a function that returns the remainder form dividing 2 integers WITHOUT USING %
- *      Ex.: modulus(7, 3) => 1
+ *      Ex: modulus(7, 3) => 1
+ *
  * @param dividend
  * @param divisor
  */
@@ -40,9 +43,51 @@ function modulus(dividend, divisor) {
 
 }
 
+/**
+ * Only Once
+ * @description: Write a function that takes a function and sets it
+ * so that it can only be called once. If it's called again it,
+ * it should return 'undefined' (a string with 'undefined' in it)
+ *      Ex: onlyOnce(someFunction) => 'someFunctions return value'
+ *          someFunction() => 'undefined'
+ *
+ * @param {function} fn: A function
+ */
+function onlyOnce(fn) {
+
+}
+
+/**
+ * Next Five
+ * @description: Write a function that takes a number and returns the next
+ * multiple of 5
+ *      Ex: nextFive(6) => 10
+ *          nextFive(25) => 30
+ *
+ * @param num
+ */
+function nextFive(num) {
+
+}
+
+/**
+ * Object Sort
+ * @description: Write a function that takes an object and sorts it
+ * alphabetically by key. Keys will always be alphabets (a-z or A-Z).
+ *      Ex: osort({z: 3, a: 1}) => {a: 1, z: 3}
+ *
+ * @param obj
+ */
+function osort(obj) {
+
+}
+
 module.exports = {
     leadZeroes: leadZeroes,
     setter: setter,
     isPrime: isPrime,
-    modulus: modulus
+    modulus: modulus,
+    onlyOnce: onlyOnce,
+    nextFive: nextFive,
+    osort: osort
 };
--- tests/roundOne.test.js
+++ tests/roundOne.test.js
@@ -1,7 +1,11 @@
 var r1 = require('../roundOne');
 
 test('moves all zeroes in an array to the beginning', function() {
-    expect(r1.leadZeroes([1,2,0,0])).toEqual([0,0,1,2])
+    var expected = [1, 2, 0, 0];
+    var actual = r1.leadZeroes(expected);
+    expect(expected).toBe(actual);
+    expect(r1.leadZeroes(expected)).toEqual([0,0,1,2]);
+    expect(r1.leadZeroes(sameArr)).toEqual([0,0,1,2])
 });
 
 test('creates an array of unique values', function() {
@@ -15,3 +19,25 @@ test('determines if a number is prime or not', function() {
 test('finds the remainder of division without %', function() {
     expect(r1.modulus(7,3)).toEqual(1)
 });
+
+test('sets a function to only be callable once and return "undefined" thereafter', function() {
+    function testfn() {
+        return 'Called'
+    }
+
+    expect(r1.onlyOnce(testfn)).toEqual('Called');
+    expect(testfn()).toEqual('undefined')
+});
+
+test('returns the next multiple of 5', function() {
+    expect(r1.nextFive(6)).toEqual(10);
+    expect(r1.nextFive(25)).toEqual(30);
+    expect(r1.nextFive(3.1)).toEqual(5);
+    expect(r1.nextFive(0)).toEqual(5);
+});
+
+
+test('sorts an object alphabetically', function() {
+    expect(r1.osort({z: 3, a: 1})).toEqual({a: 1, z: 3})
+});
+
