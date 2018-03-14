5af6ec83564b603338bc3a003147a7d46da8969f
Dev <dev@divethree.com>
2018-03-14 07:48:59 -0700

    update tests.

--- tests/roundOne.test.js
+++ tests/roundOne.test.js
@@ -4,40 +4,65 @@ test('moves all zeroes in an array to the beginning', function() {
     var expected = [1, 2, 0, 0];
     var actual = r1.leadZeroes(expected);
     expect(expected).toBe(actual);
-    expect(r1.leadZeroes(expected)).toEqual([0,0,1,2]);
-    expect(r1.leadZeroes(sameArr)).toEqual([0,0,1,2])
+    expect(r1.leadZeroes(expected)).toEqual(actual)
 });
 
-test('creates an array of unique values', function() {
-    expect(r1.setter([1,1,2,2,3,3])).toEqual([1,2,3])
+describe('setter()', function() {
+    test('creates an array of unique values 1', function() {
+        expect(r1.setter([1,1,2,2,3,3])).toEqual([1,2,3])
+    });
+    test('creates an array of unique values 2', function() {
+        expect(r1.setter(['dog', 'troll', 1, 'dog'])).toEqual(['dog', 'troll', 1])
+    });
 });
 
-test('determines if a number is prime or not', function() {
-    expect(r1.isPrime(5)).toEqual(true)
+
+describe('isPrime', function() {
+    test('determines if a number is prime or not', function() {
+        expect(r1.isPrime(5)).toEqual(true);
+    });
+    test('determines if a number is prime or not', function() {
+        expect(r1.isPrime(11)).toEqual(true);
+    });
+    test('determines if a number is prime or not', function() {
+        expect(r1.isPrime(29)).toEqual(true);
+    });
+
 });
 
-test('finds the remainder of division without %', function() {
-    expect(r1.modulus(7,3)).toEqual(1)
+describe('modulo()', function() {
+    test('finds the remainder of 7/3', function() {
+        expect(r1.modulo(7,3)).toEqual(1)
+    });
+    test('finds the remainder of 21/8', function() {
+        expect(r1.modulo(21,8)).toEqual(5)
+    });
+    test('finds the remainder of 105/12', function() {
+        expect(r1.modulo(105,12)).toEqual(9)
+    });
 });
 
-test('sets a function to only be callable once and return "undefined" thereafter', function() {
-    function testfn() {
-        return 'Called'
-    }
+describe('nextFive()', function() {
+    test('returns the next multiple of 5', function() {
+        expect(r1.nextFive(6)).toEqual(10);
+    });
+    test('returns next 5 after 25', function() {
 
-    expect(r1.onlyOnce(testfn)).toEqual('Called');
-    expect(testfn()).toEqual('undefined')
+        expect(r1.nextFive(25)).toEqual(30);
+    });
+    test('returns next 5 after 3.1', function() {
+
+        expect(r1.nextFive(3.1)).toEqual(5);
+    });
+    test('returns next 5 after 0', function() {
+        expect(r1.nextFive(0)).toEqual(5);
+    })
 });
 
-test('returns the next multiple of 5', function() {
-    expect(r1.nextFive(6)).toEqual(10);
-    expect(r1.nextFive(25)).toEqual(30);
-    expect(r1.nextFive(3.1)).toEqual(5);
-    expect(r1.nextFive(0)).toEqual(5);
-});
 
 
 test('sorts an object alphabetically', function() {
-    expect(r1.osort({z: 3, a: 1})).toEqual({a: 1, z: 3})
+    expect(r1.osort({z: 3, a: 1})).toEqual({a: 1, z: 3});
+    expect(r1.osort({marketing: true, email: false, phone: false})).toEqual({email: false, marketing: true, phone: false})
 });
 
